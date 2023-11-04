edc644ec3b22b6bfbc65533d57ced1f65f027f72
Tim Grekov <tgrekov@students.hive.fi>
2023-11-04 08:05:09 +0200

    Strlcat !dstsize returns src len, no subtraction of size_t types in checks

--- ft_strlcat.c
+++ ft_strlcat.c
@@ -6,7 +6,7 @@
 /*   By: tgrekov <tgrekov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/24 17:59:54 by tgrekov           #+#    #+#             */
-/*   Updated: 2023/10/25 13:53:07 by tgrekov          ###   ########.fr       */
+/*   Updated: 2023/11/04 08:04:13 by tgrekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,14 +18,14 @@ size_t	ft_strlcat(char *dst, const char *src, size_t dstsize)
 	size_t	dst_len;
 	size_t	i;
 
-	if (!dstsize)
-		return (0);
 	src_len = ft_strlen(src);
+	if (!dstsize)
+		return (src_len);
 	dst_len = ft_strlen(dst);
 	if (dstsize < dst_len)
 		return (src_len + dstsize);
 	i = 0;
-	while (src[i] && i < (dstsize - dst_len - 1))
+	while (src[i] && (i + dst_len + 1) < dstsize)
 	{
 		dst[dst_len + i] = src[i];
 		i++;
