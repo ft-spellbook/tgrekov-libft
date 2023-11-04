836fbfadd203acd7cf2263119880fa1e174247b3
Tim Grekov <tgrekov@students.hive.fi>
2023-11-05 00:45:42 +0200

    Swapped decimal character representation to character literal, added Doxygen documentation comments

--- ft_isprint.c
+++ ft_isprint.c
@@ -6,11 +6,24 @@
 /*   By: tgrekov <tgrekov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:35:10 by tgrekov           #+#    #+#             */
-/*   Updated: 2023/10/23 14:35:59 by tgrekov          ###   ########.fr       */
+/*   Updated: 2023/11/04 22:24:41 by tgrekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file ft_isprint.c
+ * @dontinclude ft_isprint.c
+ * @line /\* *********
+ * @until /\* *********
+ */
+
+/**
+ * @brief Is @p c a printable character
+ * 
+ * @param[in] c Character to test
+ * @retval int @p 1 if @p c is within the range '<tt> </tt>' - @p ~
+ */
 int	ft_isprint(int c)
 {
-	return (c >= ' ' && c < 127);
+	return (c >= ' ' && c <= '~');
 }
