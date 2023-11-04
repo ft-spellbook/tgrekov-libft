cb11a6b08594783d0cdf383386bfaae0280864d1
Tim Grekov <tgrekov@students.hive.fi>
2023-11-05 00:46:10 +0200

    Removed unnecessary variable, added Doxygen documentation comments

--- ft_memset.c
+++ ft_memset.c
@@ -6,18 +6,31 @@
 /*   By: tgrekov <tgrekov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:43:01 by tgrekov           #+#    #+#             */
-/*   Updated: 2023/10/25 13:22:52 by tgrekov          ###   ########.fr       */
+/*   Updated: 2023/11/04 22:47:09 by tgrekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+/**
+ * @file ft_memset.c
+ * @dontinclude ft_memset.c
+ * @line /\* *********
+ * @until /\* *********
+ */
+
 #include "libft.h"
 
+/**
+ * @brief Fills @p len bytes of byte string @p b with value @c
+ * 
+ * @param[in, out] b Pointer to start of byte string
+ * @param[in] c Integer representation of value to write
+ * (cast to unsigned char)
+ * @param[in] len Number of bytes to write
+ * @retval void* @p b
+ */
 void	*ft_memset(void *b, int c, size_t len)
 {
-	size_t			i;
-
-	i = 0;
-	while (i < len)
-		((unsigned char *) b)[i++] = c;
+	while (len--)
+		((unsigned char *) b)[len] = c;
 	return (b);
 }
