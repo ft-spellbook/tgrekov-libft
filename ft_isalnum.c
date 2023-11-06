98212b8a042a673cda8a178ba94c21539c0b6d0e
Tim Grekov <tgrekov@students.hive.fi>
2023-11-06 15:36:35 +0200

    Added @ref tags where appropriate

--- ft_isalnum.c
+++ ft_isalnum.c
@@ -6,7 +6,7 @@
 /*   By: tgrekov <tgrekov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/23 13:29:39 by tgrekov           #+#    #+#             */
-/*   Updated: 2023/11/04 22:24:45 by tgrekov          ###   ########.fr       */
+/*   Updated: 2023/11/06 13:36:53 by tgrekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,8 +23,8 @@
  * @brief Is c an alphanumeric character
  * 
  * @param[in] c Character to test
- * @retval int @p 1 if either @ref ft_isalpha "ft_isalpha"
- * or @ref ft_isdigit "ft_isdigit", otherwise @p 0.
+ * @retval int <tt>@ref ft_isalpha"ft_isalpha"(c)
+ * || @ref ft_isdigit"ft_isdigit"(c)</tt>
  */
 int	ft_isalnum(int c)
 {
