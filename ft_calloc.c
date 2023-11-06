98a072ec8fe933a32277fd4bc4f9bff01d20704a
Tim Grekov <tgrekov@students.hive.fi>
2023-11-06 19:17:55 +0200

    Fixed mistaken logic in calloc regarding count and / or size being 0

--- ft_calloc.c
+++ ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: tgrekov <tgrekov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/25 14:33:28 by tgrekov           #+#    #+#             */
-/*   Updated: 2023/11/06 13:28:40 by tgrekov          ###   ########.fr       */
+/*   Updated: 2023/11/06 19:16:56 by tgrekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,9 +27,8 @@
  * @param[in] size Size of each object
  * @retval void* Freeable pointer to allocated memory after it is
  * @ref ft_bzero "filled with zeroes"  
- * If @a only one of @p count or @p size are \p 0, returns \p NULL.  
- * If @a both @p count @a and @p size are \p 0,
- * returns <tt>@ref ft_calloc"ft_calloc"(1, 1)</tt>.  
+ * If either @p count or @p size are \p 0, returns
+ * <tt>@ref ft_calloc"ft_calloc"(1, 1)</tt>.  
  * If the multiplication of @p count and @p size overflows, returns \p NULL.
  */
 void	*ft_calloc(size_t count, size_t size)
@@ -37,10 +36,8 @@ void	*ft_calloc(size_t count, size_t size)
 	void	*ptr;
 	size_t	bsize;
 
-	if (!count && !size)
-		return (ft_calloc(1, 1));
 	if (!count || !size)
-		return (0);
+		return (ft_calloc(1, 1));
 	bsize = count * size;
 	if (bsize / count != size)
 		return (0);
