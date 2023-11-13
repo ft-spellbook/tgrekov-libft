d7e7524079e5d62f94a11bd6824a00ab1a775e3f
Tim Grekov <tgrekov@students.hive.fi>
2023-11-13 22:12:39 +0200

    Rewrote split in a human manner

--- ft_split.c
+++ ft_split.c
@@ -6,7 +6,7 @@
 /*   By: tgrekov <tgrekov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/26 15:41:34 by tgrekov           #+#    #+#             */
-/*   Updated: 2023/11/09 20:12:39 by tgrekov          ###   ########.fr       */
+/*   Updated: 2023/11/13 22:11:57 by tgrekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,31 +19,20 @@
 
 #include "libft.h"
 
-static void	skip_chars(char const **s, char c)
-{
-	while (**s && **s == c)
-		(*s)++;
-}
-
 static size_t	count_sections(char const *s, char c)
 {
 	size_t	i;
+	size_t	sections;
 
-	if (!*s)
-		return (0);
-	i = 1;
-	while (*s)
+	sections = 0;
+	i = 0;
+	while (s[i])
 	{
-		if (*s == c)
-		{
-			skip_chars(&s, c);
-			if (*s)
-				i++;
-		}
-		else
-			s++;
+		if (s[i] != c && (!i || s[i - 1] == c))
+			sections++;
+		i++;
 	}
-	return (i);
+	return (sections);
 }
 
 static char	**free_all(char **arr)
@@ -55,6 +44,20 @@ static char	**free_all(char **arr)
 		free(arr[i++]);
 	free(arr);
 	return (0);
+}
+
+static size_t	end_of_word(char const *s, char c)
+{
+	size_t	i;
+
+	i = 0;
+	while (s[i])
+	{
+		if (s[i] == c)
+			return (i);
+		i++;
+	}
+	return (ft_strlen(s));
 }
 
 /**
@@ -72,23 +75,21 @@ char	**ft_split(char const *s, char c)
 	size_t	arr_i;
 	size_t	len;
 
-	if (!s)
-		return (0);
-	skip_chars(&s, c);
+	while (*s && *s == c)
+		s++;
 	arr = ft_calloc(count_sections(s, c) + 1, sizeof(char *));
 	if (!arr)
 		return (0);
 	arr_i = 0;
 	while (*s)
 	{
-		len = 0;
-		while (s[len] && s[len] != c)
-			len++;
+		len = end_of_word(s, c);
 		arr[arr_i] = ft_substr(s, 0, len);
 		if (!arr[arr_i++])
 			return (free_all(arr));
 		s += len;
-		skip_chars(&s, c);
+		while (*s && *s == c)
+			s++;
 	}
 	return (arr);
 }
