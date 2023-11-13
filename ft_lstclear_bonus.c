c579313815a2085250e6ae8e050e1fee86b0d3b3
Tim Grekov <tgrekov@students.hive.fi>
2023-11-13 22:12:52 +0200

    Comment length

--- ft_lstclear_bonus.c
+++ ft_lstclear_bonus.c
@@ -6,7 +6,7 @@
 /*   By: tgrekov <tgrekov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 01:38:22 by tgrekov           #+#    #+#             */
-/*   Updated: 2023/11/09 20:52:40 by tgrekov          ###   ########.fr       */
+/*   Updated: 2023/11/13 22:10:23 by tgrekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -23,7 +23,8 @@
  * @brief Delete every @ref s_list "node" in a list
  * along with it's @ref s_list::content "content"
  * 
- * @param[in, out] lst Pointer to a pointer to the first @ref s_list "node" in the list
+ * @param[in, out] lst Pointer to a pointer to the first @ref s_list "node"
+ * in the list
  * @param[in] del Function used to properly handle the deletion of the
  * @ref s_list "node"'s @ref s_list::content "content"
  */
