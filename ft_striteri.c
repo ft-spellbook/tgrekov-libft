f4b48979631a2ff41fcec2a9c5109109e4ec66f4
Tim Grekov <tgrekov@students.hive.fi>
2023-11-15 23:40:44 +0200

    Input protection

--- ft_striteri.c
+++ ft_striteri.c
@@ -6,7 +6,7 @@
 /*   By: tgrekov <tgrekov@student.hive.fi>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/10/28 00:00:09 by tgrekov           #+#    #+#             */
-/*   Updated: 2023/11/09 20:09:45 by tgrekov          ###   ########.fr       */
+/*   Updated: 2023/11/15 23:40:30 by tgrekov          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -32,7 +32,7 @@ void	ft_striteri(char *s, void (*f)(unsigned int, char *))
 {
 	unsigned int	len;
 
-	if (!s)
+	if (!s || !f)
 		return ;
 	len = ft_strlen(s);
 	while (len--)
