9c5fea90182182c4bb40eb6339780f5f32453774
Tim Grekov <tgrekov@students.hive.fi>
2023-11-24 09:52:13 +0200

    Script clean submission

--- ft_strchr.c
+++ ft_strchr.c
@@ -10,23 +10,6 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-/**
- * @file ft_strchr.c
- * @dontinclude ft_strchr.c
- * @line /\* *********
- * @until /\* *********
- */
-
-/**
- * @brief Finds first occurence of character @p c in string @p s
- * 
- * Check is done as @p char
- * 
- * @param[in] s Null-terminated string haystack to search
- * @param[in] c int needle to find
- * @retval char* Pointer to first occurence of needle @p c in haystack @p s.  
- * @p NULL if none is found before the end of the string.
- */
 char	*ft_strchr(const char *s, int c)
 {
 	while (*s)
